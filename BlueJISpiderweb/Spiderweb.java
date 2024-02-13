1bf43ea599a5f9b96238671496d4df5c0e19b186
Diego Cardenas <diegosp778@gmail.com>
2024-02-13 00:21:16 -0500

    ADD: Archivos iniciales

---
+++ BlueJISpiderweb/Spiderweb.java
@@ -0,0 +1,67 @@
+
+/**
+ * Write a description of class Spiderweb here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Spiderweb{
+    private int strands;
+    private int radio;
+    
+    /**
+     * 
+     */
+    public Spiderweb(int strands, int radio){
+        
+    }
+    
+    /**
+     * 
+     */
+    public void addBridge(String color, int distance, int firstStrand){
+    
+    }
+    
+    /**
+     * 
+     */
+    public void relocateBridge(String color, int distance){
+    
+    }
+    
+    /**
+     * 
+     */
+    public void delBridge(String color){
+    
+    }
+    
+    /**
+     * 
+     */
+    public void addSpot(String color, int strand){
+    
+    }
+    
+    /**
+     * 
+     */
+    public void delSpot(String color){
+    
+    }
+    
+    /**
+     * 
+     */
+    public void spiderSit(int strand){
+    
+    }
+    
+    /**
+     * 
+     */
+    public void spiderWalk(boolean advance){
+    
+    }
+}
